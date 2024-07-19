cf014af7dcbd9aca46dc8a2c55157f3b0dcb6741
Hoeyun <yang3522@gmail.com>
2024-07-19 18:26:10 +0900

    Modal 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -54,6 +54,17 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>7월 19일 발행</p>
       </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세 내용</p>
     </div>
   );
 }
