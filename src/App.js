dd85eff457d3c0791ddef296e08ceed39b8a6786
Hoeyun <yang3522@gmail.com>
2024-07-19 17:29:35 +0900

    숙제)버튼 누르면 바뀌게 하는 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let post = "강남 우동 맛집";
   let [글제목, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState(0);
+  let [성별, 성별변경] = useState(글제목[0]);
 
   return (
     <div className="App">
@@ -16,7 +17,14 @@ function App() {
       </div>
       <div className="list">
         <h4>
-          {글제목[0]}{" "}
+          {성별}
+          <span
+            onClick={() => {
+              성별변경("여자 코트 추천");
+            }}
+          >
+            ❤
+          </span>
           <span
             onClick={() => {
               따봉변경(따봉 + 1);
