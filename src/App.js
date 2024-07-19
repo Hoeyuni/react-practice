6f031b75a1f6bdab784ebfbeaef00b8b90963132
Hoeyun <yang3522@gmail.com>
2024-07-19 17:48:57 +0900

    state가 array/object면 [...변수]를 사용한다

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState(0);
-  let [성별, 성별변경] = useState(글제목[0]);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자 코트 추천";
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
       <div className="list">
         <h4>
-          {성별}
-          <span
-            onClick={() => {
-              성별변경("여자 코트 추천");
-            }}
-          >
-            ❤
-          </span>
+          {글제목[0]}
           <span
             onClick={() => {
               따봉변경(따봉 + 1);
