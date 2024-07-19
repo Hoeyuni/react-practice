b965d8f1419ee7772abc6ce5d20828995ca06938
Hoeyun <yang3522@gmail.com>
2024-07-19 17:26:10 +0900

    onClick 쓰는 법 및 state 변경하는 법

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
@@ -5,6 +7,7 @@ import { useState } from "react";
 function App() {
   let post = "강남 우동 맛집";
   let [글제목, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -12,7 +15,17 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h4>
         <p>7월 19일 발행</p>
       </div>
       <div className="list">
